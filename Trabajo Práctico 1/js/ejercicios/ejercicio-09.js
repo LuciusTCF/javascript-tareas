5628598d5888c4769ee091f6093c36bcf1e823e0
Lux <lucaschcobarferreyra@gmail.com>
2023-07-11 22:57:03 -0300

    Completado el punto 9 del Trabajo Práctico 1 con lo aprendido en la clase de Arrays

--- Trabajo Práctico 1/js/ejercicios/ejercicio-09.js
+++ Trabajo Práctico 1/js/ejercicios/ejercicio-09.js
@@ -1,9 +1,31 @@
-let frase = prompt(`Escriba una frase para ver sus vocales.`);
+// let frase = prompt("Escriba una frase para ver sus vocales.");
+// let fraseSinEspacio = frase.match(/[aeiou]/gi).join("")
 
-let fraseSinEspacio = frase.match(/[aeiou]/gi).join("")
-
-document.write(fraseSinEspacio);
-
+// document.write(fraseSinEspacio);
 
 
 //Como esta tarea fue quitada del TP, intenté resolverla con ayuda de internet, donde encontré la solución y luego me puse a ver acerca de Regular Expresions y Flags.
+
+
+
+
+
+//Tarea con lo aprendido con Arrays
+
+
+let frase = prompt("Escriba una frase para ver sus vocales.");
+let vocales = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+let vocalesFrase = [];
+do {
+    if (!frase) {
+        alert("No ingresó una frase.");
+        frase = prompt("Escriba una frase para ver sus vocales.");
+    }
+} while (!frase) {
+    for (let index = 0; index < frase.length; index++) {
+        if (vocales.includes(frase[index])) {
+            vocalesFrase.push(frase[index]);
+        }
+    }
+}
+document.write(vocalesFrase.join(" | "));
